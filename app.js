6ca5e0aa198becb3ae79eebef32a67f28dc2cb68
Prafull Gururani <praguru14@gmail.com>
2020-02-11 18:01:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 20 points on GLOBAL score wins the game
 
 */
+alert("GAME RULES: \n1. The game has 2 players, playing in rounds.\n2.In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score. \n3. BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn. \n4. The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn. \n5. The first player to reach 20 points on GLOBAL score wins the game");
 var scores,roundScores,activePlayer,lastScore;
 init();
 document.querySelector('.btn-roll').addEventListener('click',function () {
